0a4ef2b8686d7d152413dc3ac3affc98b1f3324f
SKhrebtan <shrebtan@gmail.com>
2023-08-04 16:03:41 +0300

    add npm className cssmodule

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,13 +1,25 @@
+import classNames from 'classnames/bind';
+import styles from './List.module.css'
+    
+
 export default function List({ options, onCheckbox, onClick }) {
+    
     return (
         <ul>
-            {options.map(({name, number, id,status, checked}) => {
+            {options.map(({ name, number, id, status, checked }) => {
+                const cx = classNames.bind(styles)
+    
+                let className = cx({
+        text: true,
+      text_active: checked === true
+    });
                 return (
                     <li key={id}>
-                        <p>Ім'я: {name}</p>
-                        <p>Номер: {number}</p>
-                        <p>Статус: {status}</p>
+                        <p className={className}>Ім'я: {name}</p>
+                        <p className={className}>Номер: {number}</p>
+                        <p className={className}>Статус: {status}</p>
                         <input type="checkbox"
+                            className="input"
                         checked={checked}
                         onChange={()=> onCheckbox(id)}/>
                         <button type="button" disabled={!checked} onClick={()=>onClick(id)}>Delete</button>
